5af8e3aec2d5095bdee1b44f448f92f9546f7529
BiggestChris <stylianouchristopher8@gmail.com>
2023-10-29 11:14:22 +0000

    Removing static Menu items

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = initializeApp(appSettings);
 const database = getDatabase(app);
 const pizzaInDB = ref(database, "Menu");
 
-const testBlock = document.getElementById("test-block");
+const menuBlock = document.getElementById("menu-block");
 
 onValue(pizzaInDB, function(snapshot) {
 
@@ -19,7 +19,7 @@ onValue(pizzaInDB, function(snapshot) {
     for (let i = 0; i < itemsArray.length; i++) {
         let item = itemsArray[i][1];
 
-        testBlock.innerHTML += 
+        menuBlock.innerHTML += 
         `
         <div class="menu-line-one">
             <h3 class="item-name">${item.name}</h3>
@@ -31,29 +31,5 @@ onValue(pizzaInDB, function(snapshot) {
 
     }
 
-    /* let item1 = itemsArray[0][1];
-
-    testBlock.innerHTML += 
-        `
-        <div class="menu-line-one">
-            <h3 class="item-name">${item1.name}</h3>
-            <h3 class="item-price">£${item1.price}</h3>
-        </div>
-        <p class="item-description">${item1.description}</p>
-        ${item1.code}
-        `
-
-    let item2 = itemsArray[1][1];
-
-    testBlock.innerHTML += 
-        `
-        <div class="menu-line-one">
-            <h3 class="item-name">${item2.name}</h3>
-            <h3 class="item-price">£${item2.price}</h3>
-        </div>
-        <p class="item-description">${item2.description}</p>
-        ${item2.code}
-        `
-    */
 
 });
